0315e9d70eb73bded2eb5913f57427e866d0ebf3
Moreels Mario <mario.moreels@howest.student.be>
2021-01-07 13:00:06 +0100

    Create CalculateStudentStatistics Override

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -12,6 +12,12 @@ namespace GradeBook.GradeBooks
             Type = GradeBookType.Ranked;
         }
 
+        public override void CalculateStatistics()
+        {
+            if (Students.Count < 5)
+                throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
+        }
+
         public override char GetLetterGrade(double averageGrade)
         {
             if (Students.Count < 5)

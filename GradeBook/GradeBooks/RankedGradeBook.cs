6799e59086bff66a32a486a6692fe839e56ecf2a
Moreels Mario <mario.moreels@howest.student.be>
2021-01-07 13:05:20 +0100

    error fix 2

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -16,7 +16,7 @@ namespace GradeBook.GradeBooks
         {
             if (Students.Count < 5)
                 throw new InvalidOperationException("Ranked grading requires at least 5 students with grades in order to properly calculate a student's overall grade.");
-            base.CalculateStatistics();
+            else base.CalculateStatistics();
         }
 
         public override char GetLetterGrade(double averageGrade)
